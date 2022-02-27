cf11af8fb85bf93040d423e87fc1eb5726a1332d
Pacinini <72420688+Pacinini@users.noreply.github.com>
2022-02-26 21:24:20 -0600

    Add project files.

---
+++ ConsoleAppTest1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleAppTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string function = "";
+            double valueOne;
+            double valueTwo;
+
+            while (function != "exit")
+            {
+                Console.Write("Press operator [+, -, *, /] or type 'exit' to terminate program: ");
+                function = Console.ReadLine();
+
+                //if (function != "exit")
+                //{
+                    Console.Write("First number: ");
+                    valueOne = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Second Number: ");
+                    valueTwo = Convert.ToDouble(Console.ReadLine());
+                //}
+
+                if (function == "+")
+                {
+                    Console.WriteLine(valueOne + valueTwo);
+                } 
+                else if (function == "-")
+                {
+                    Console.WriteLine(valueOne - valueTwo);
+                }
+                else if (function == "*")
+                {
+                    Console.WriteLine(valueOne * valueTwo);
+                } 
+                else if (function == "/")
+                {
+                    Console.WriteLine(valueOne / valueTwo);
+                }
+            }
+
+            Console.Write("Press the 'enter' key to exit... ");
+            Console.ReadLine();
+        }
+    }
+}
